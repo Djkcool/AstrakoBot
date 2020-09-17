78fa0fa175aba4f943431b8ad1803474709908ae
Alejandro <apamarin@gmail.com>
2020-09-17 19:15:53 +0200

    admin: add link command option

--- AstrakoBot/modules/admin.py
+++ AstrakoBot/modules/admin.py
@@ -367,6 +367,7 @@ __help__ = """
  • `/pin`*:* silently pins the message replied to - add `'loud'` or `'notify'` to give notifs to users.
  • `/unpin`*:* unpins the currently pinned message
  • `/invitelink`*:* gets invitelink
+ • `/link`*:* same as /invitelink
  • `/promote`*:* promotes the user replied to
  • `/demote`*:* demotes the user replied to
  • `/settitle`*:* sets a custom title for an admin that the bot promoted
@@ -378,8 +379,7 @@ ADMINLIST_HANDLER = DisableAbleCommandHandler(["adminlist", "admins"],
 PIN_HANDLER = CommandHandler("pin", pin, filters=Filters.group)
 UNPIN_HANDLER = CommandHandler("unpin", unpin, filters=Filters.group)
 
-INVITE_HANDLER = DisableAbleCommandHandler(
-    "invitelink", invite, filters=Filters.group)
+INVITE_HANDLER = DisableAbleCommandHandler(["invitelink", "link"], invite, filters=Filters.group)
 
 PROMOTE_HANDLER = DisableAbleCommandHandler("promote", promote)
 DEMOTE_HANDLER = DisableAbleCommandHandler("demote", demote)
